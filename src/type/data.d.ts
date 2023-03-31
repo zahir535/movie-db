4daa91be886e43e33548491d3e4c660cc004e072
rgzahir <zahir.ayob@regovtech.com>
2023-03-31 09:01:11 +0800

    add type & dummy data

---
+++ src/type/data.d.ts
@@ -0,0 +1,17 @@
+interface IDataItem {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: false;
+  vote_average: number;
+  vote_count: number;
+  popularity: number;
+  original_name?: undefined;
+}
